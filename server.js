14e4fdc0ec6addefaa42c2d757648595cb39a212
FVL <fvl@aarvakuzhu.com>
2026-05-01 00:26:49 +0000

    Update schedule dates: May 17, Jun 14, Jul 19

--- server.js
+++ server.js
@@ -467,9 +467,9 @@ const S3_TEAMS = [
 
 // Month schedule: May–Oct 2026 (Sundays)
 const S3_MONTHS = [
-  { month:1, label:'May 2026',      date:'Sun 3 May 2026' },
-  { month:2, label:'June 2026',     date:'Sun 7 Jun 2026' },
-  { month:3, label:'July 2026',     date:'Sun 5 Jul 2026' },
+  { month:1, label:'May 2026',      date:'Sat 17 May 2026' },
+  { month:2, label:'June 2026',     date:'Sat 14 Jun 2026' },
+  { month:3, label:'July 2026',     date:'Sat 19 Jul 2026' },
   { month:4, label:'August 2026',   date:'Sun 2 Aug 2026' },
   { month:5, label:'September 2026',date:'Sun 6 Sep 2026' },
   { month:6, label:'October 2026',  date:'Sun 4 Oct 2026' },
